0aaf1c80031f8a4761bdaa76d8be0b564ba014fb
shahzan01 <m.shahzan2003@gmail.com>
2024-12-28 18:19:36 +0530

    first

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
   resolve: {
