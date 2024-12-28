7d2eca2913626d3c2af3c141667cbc1553d4e7a7
shahzan01 <m.shahzan2003@gmail.com>
2024-12-28 19:16:40 +0530

    js remove

--- src/context/DarkModeContext.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DarkModeContext = void 0;
-var react_1 = require("react");
-exports.DarkModeContext = (0, react_1.createContext)(undefined);
--- src/context/SolanaChainContext.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SolanaChainContext = void 0;
-var react_1 = require("react");
-exports.SolanaChainContext = (0, react_1.createContext)(undefined);
